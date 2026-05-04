3913c4c28bd0d23b5b4c1f89e16f8ec423a87542
abhijan23 <mallickabhijan23@gmil.com>
2026-05-04 18:23:30 +0530

    Initial Commit

---
+++ src/main/java/com/voting/online_voting_system/dto/LoginRequest.java
@@ -0,0 +1,20 @@
+package com.voting.online_voting_system.dto;
+
+public class LoginRequest {
+    private String email;
+    private String password;
+
+    //Getter & Setter
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+    public String getPassword(){
+        return password;
+    }
+    public void setPassword(String password){
+        this.password = password;
+    }
+}
