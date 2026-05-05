a3feadadcf414d32ea69573cd3afd5b0fed940b5
abhijan23 <mallickabhijan23@gmil.com>
2026-05-06 00:03:22 +0530

    Logic Update

--- src/main/java/com/voting/online_voting_system/service/UserService.java
+++ src/main/java/com/voting/online_voting_system/service/UserService.java
@@ -1,8 +1,10 @@
 package com.voting.online_voting_system.service;
 
+import com.voting.online_voting_system.dto.RegisterRequest;
 import com.voting.online_voting_system.entity.User;
 import com.voting.online_voting_system.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,6 +12,14 @@ public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public User registerUser(RegisterRequest request){
+        User user = new User();
+        
+    }
 
     public User saveUser(User user){
         return userRepository.save(user);
